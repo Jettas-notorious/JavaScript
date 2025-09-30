b9401548953b9a78825a12c0d32c1370614fd994
Jettas-notorious <jetrocleophas1@gmail.com>
2025-09-30 10:12:05 +0200

    dwddhqcc

---
+++ Module exercises/Mod2/Task.js
@@ -0,0 +1,17 @@
+"use strict"
+var rose,Lily,tulip,rose_price,lily_price,tulip_price,total_price;
+rose="Rose";
+Lily="Lily";
+tulip="Tulip";
+rose_price=70;
+lily_price=50;
+tulip_price=120;
+total_price=rose_price*8+lily_price*10+tulip_price*2;
+
+
+
+console.log(rose,"-","unit price:8","Quantity:",rose_price,"value:",rose_price*8);
+console.log(Lily,"-","unit price:10","Quantity:",lily_price,"value:",lily_price*10);
+console.log(tulip,"-","unit price:2","Quantity:",tulip_price,"value:",tulip_price*2);
+console.log("Total:",total_price);
+
---
+++ Module exercises/Mod2/app_varaiables.js
@@ -0,0 +1,15 @@
+"use strict"
+let name1,name2,name3,p1,p2,p3,e1,e2,e3;
+name1="Maxwell Wright";
+name2="Raja Vilarreal";
+name3="Helen Richards";
+p1=7196495;
+p2=8663982895;
+p3=80001111;
+e1="Curabitur.egestas.nunc@nonummyac.co.uk";
+e2="posuere.vulputate@sed.com";
+e3="libero@convallis.edu";
+
+console.log(name1,"/(0191)"+p1,"/"+e1);
+console.log(name2,"/0"+p2,"/"+e2,);
+console.log(name3,"/0"+p3,"/"+e3);
